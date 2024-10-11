29f5c9678990c20852d300c34c1ae0d870056e01
Đorđe Grbić <968662+djole@users.noreply.github.com>
2024-10-11 17:02:22 +0200

    Add comments to the Fruit script

--- platformer/Assets/FruitScript.cs
+++ platformer/Assets/FruitScript.cs
@@ -11,13 +11,29 @@ public class FruitScript : MonoBehaviour
     public string fruitTag = "Apple_0"; 
     void Start()
     {
-
+        // Get all the "Sprite" resources from the "Resource" folder in the Sprite[] array
         Sprite[] allSpritesFromFolder = Resources.LoadAll<Sprite>("");
+        // Iterate over all the sprites and add them into the dictionary so we can refer to them by their name
         foreach (Sprite sprite in allSpritesFromFolder){
             sprites.Add(sprite.name, sprite);
         }
+        // Get the renderer component
         SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        // Apply the sprite from the dictionary to the renderer sprite
         renderer.sprite = sprites[fruitTag];
+
+        // *******************************************************
+        // Alternatively, a more elegant solution without the dictionary is:
+        // *******************************************************
+        // Sprite[] allSpritesFromFolder = Resources.LoadAll<Sprite>("");
+        // foreach (Sprite sprite in allSpritesFromFolder){
+        //    if (sprite.name == fruitTag){
+        //        SpriteRenderer renderer = gameObject.GetComponent<SpriteRenderer>();
+        //        renderer.sprite = sprite;
+        //    }
+        //    sprites.Add(sprite.name, sprite);
+        //}
+        //
 
     }
 
